84a5b9a62b00c11ac4e716f4574a99e28877ae44
kgoins <kgoins@github.com>
2018-06-03 15:55:43 -0500

    added .vscode project file and overhauled payloadGen to use argparse

--- payloadGen.py
+++ payloadGen.py
@@ -2,41 +2,43 @@
 
 import os
 import sys
+
+import argparse
+
 from binascii import unhexlify
 from binascii import hexlify
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--paddinglen', help="Number of padding chars before payload")
+parser.add_argument('-p', '--payload', help="Payload in hex")
+parser.add_argument('-o', '--outfile', help="Output file")
+
+def convertPayload(payload):
+    if (payload.startswith("0x")):
+        payload = payload.strip("0x")
+
+    reversedPayload = unhexlify(payload)[::-1]
+    return reversedPayload
+
 def main():
-    argc = len(sys.argv)
+    args = parser.parse_args()
 
-    if (argc == 1):
-        print "Usage: "
-        print "./payloadGen.py [payloadLen] [payloadVal] [outfile]"
-        print "ex) ./payloadGen.py 40 0x61626364"
+    padding = ""
+    if (args.paddinglen):
+        padding = 'A' * int(args.paddinglen)
 
-    padding = 'A' * int(sys.argv[1])
-
-    if (argc < 3):
-        print padding
-        return
-
-    payloads = sys.argv[2].split(',')
-
-    finalPayload = ""
-    if (argc < 4):
-        for payload in payloads:
-            reversedPayload = unhexlify(payload)[::-1]
-            print reversedPayload
-            finalPayload += reversedPayload
-
-        print type(finalPayload)
-
+    if (args.payload):
+        payload = convertPayload(args.payload)
     else:
-        outfileName = sys.argv[3]
-        with open(outfileName, 'wb') as outfile:
+        payload = ""
+        
+    if (args.outfile):
+        with open(args.outfile, 'wb') as outfile:
             outfile.write(padding)
-            for payload in payloads:
-                reversedPayload = unhexlify(payload)[::-1]
-                outfile.write(reversedPayload)
+            outfile.write(payload)
+    else:
+        print padding + payload
 
 if __name__ == '__main__':
     main()
